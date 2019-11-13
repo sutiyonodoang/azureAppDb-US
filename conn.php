09f4eb444db308ed7d70ba3dcbd5991a61990f3c
tiyo <sutiyonodoang@gmail.com>
2019-11-14 06:36:45 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -13,8 +13,11 @@ catch (PDOException $e) {
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "sutiyonodoang", "pwd" => "Sutiyono@270382", "Database" => "PustakaPublik", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0, "ReturnDatesAsStrings"=> true);
 
+// failover read/write listener //
+$serverName = "tcp:failoverdb.database.windows.net,1433";
 // $serverName = "tcp:dicodingdbserver1.database.windows.net,1433";
-$serverName = "tcp:pustakapublik.database.windows.net,1433";
+// $serverName = "tcp:pustakapublik.database.windows.net,1433";
+
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
